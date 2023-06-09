6dcfae0f54fcdc2a330d1158121cb41ba08d588e
Sarrazin Florent <florent.sarrazin@outlook.fr>
2023-06-09 08:48:14 +0200

    scss changes

--- public/assets/index.js
+++ public/assets/index.js
@@ -21,3 +21,24 @@ function submitForm() {
 }
 
 // --- Btn like ---
+
+// --- Carrousel ---
+
+document.addEventListener('DOMContentLoaded', function () {
+  const slides = document.querySelectorAll('.slide');
+  let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide) => slide.classList.remove('active'));
+  slides[index].classList.add('active');
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000);
+});
+
+// --- Carrousel ---
